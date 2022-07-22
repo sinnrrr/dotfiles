0c1f98f84a7e7f22d8d0f25a37d6a3615d07584b
Dmytro Soltysiuk <dimasoltysiuk@gmail.com>
2022-07-22 11:41:21 +0200

    feat: tailwindcss lsp

--- lvim/.config/lvim/ftplugin/typescript.lua
+++ lvim/.config/lvim/ftplugin/typescript.lua
@@ -1,95 +1,171 @@
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "denols" })
 require("lvim.lsp.manager").setup("denols", {
-  cmd = {
-    "deno",
-    "lsp",
-  },
-  filetypes = {
-    "javascript",
-    "javascriptreact",
-    "javascript.jsx",
-    "typescript",
-    "typescriptreact",
-    "typescript.tsx",
-  },
-  init_options = {
-    enable = true,
-    lint = true,
-    unstable = false,
-  },
-  root_dir = require("lspconfig").util.root_pattern(
-    "deno.json",
-    "import_map.json",
-    "script.config.ts",
-    "script.config.js"
-  ),
+	cmd = {
+		"deno",
+		"lsp",
+	},
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
+	init_options = {
+		enable = true,
+		lint = true,
+		unstable = false,
+	},
+	root_dir = require("lspconfig").util.root_pattern(
+		"deno.json",
+		"import_map.json",
+		"script.config.ts",
+		"script.config.js"
+	),
 })
+
+local tailwindcss_opts = {
+	cmd = { "tailwindcss-language-server", "--stdio" },
+	filetypes = {
+		"aspnetcorerazor",
+		"astro",
+		"astro-markdown",
+		"blade",
+		"django-html",
+		"htmldjango",
+		"edge",
+		"eelixir",
+		"ejs",
+		"erb",
+		"eruby",
+		"gohtml",
+		"haml",
+		"handlebars",
+		"hbs",
+		"html",
+		"html-eex",
+		"heex",
+		"jade",
+		"leaf",
+		"liquid",
+		"markdown",
+		"mdx",
+		"mustache",
+		"njk",
+		"nunjucks",
+		"php",
+		"razor",
+		"slim",
+		"twig",
+		"css",
+		"less",
+		"postcss",
+		"sass",
+		"scss",
+		"stylus",
+		"sugarss",
+		"javascript",
+		"javascriptreact",
+		"reason",
+		"rescript",
+		"typescript",
+		"typescriptreact",
+		"vue",
+		"svelte",
+	},
+	root_dir = require("lspconfig").util.root_pattern(
+		"tailwind.config.js",
+		"tailwind.config.ts",
+		"postcss.config.js",
+		"postcss.config.ts",
+		"package.json",
+		"node_modules",
+		".git"
+	),
+	settings = {
+		tailwindCSS = {
+			classAttributes = { "class", "className", "classList", "ngClass" },
+			lint = {
+				cssConflict = "warning",
+				invalidApply = "error",
+				invalidConfigPath = "error",
+				invalidScreen = "error",
+				invalidTailwindDirective = "error",
+				invalidVariant = "error",
+				recommendedVariantOrder = "warning",
+			},
+			validate = true,
+		},
+	},
+}
+require("lvim.lsp.manager").setup("tailwindcss", tailwindcss_opts)
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "tsserver" })
 require("lvim.lsp.manager").setup("tsserver", {
-  -- Needed for inlayHints. Merge this table with your settings or copy
-  -- it from the source if you want to add your own init_options.
-  init_options = require("nvim-lsp-ts-utils").init_options,
-  --
-  on_attach = function(client, bufnr)
-    local ts_utils = require("nvim-lsp-ts-utils")
+	-- Needed for inlayHints. Merge this table with your settings or copy
+	-- it from the source if you want to add your own init_options.
+	init_options = require("nvim-lsp-ts-utils").init_options,
+	--
+	on_attach = function(client, bufnr)
+		local ts_utils = require("nvim-lsp-ts-utils")
 
-    -- defaults
-    ts_utils.setup({
-      debug = false,
-      disable_commands = false,
-      enable_import_on_completion = true,
+		-- defaults
+		ts_utils.setup({
+			debug = false,
+			disable_commands = false,
+			enable_import_on_completion = true,
 
-      -- import all
-      import_all_timeout = 5000, -- ms
-      -- lower numbers = higher priority
-      import_all_priorities = {
-        same_file = 1, -- add to existing import statement
-        local_files = 2, -- git files or files with relative path markers
-        buffer_content = 3, -- loaded buffer content
-        buffers = 4, -- loaded buffer names
-      },
-      import_all_scan_buffers = 100,
-      import_all_select_source = false,
-      -- if false will avoid organizing imports
-      always_organize_imports = true,
+			-- import all
+			import_all_timeout = 5000, -- ms
+			-- lower numbers = higher priority
+			import_all_priorities = {
+				same_file = 1, -- add to existing import statement
+				local_files = 2, -- git files or files with relative path markers
+				buffer_content = 3, -- loaded buffer content
+				buffers = 4, -- loaded buffer names
+			},
+			import_all_scan_buffers = 100,
+			import_all_select_source = false,
+			-- if false will avoid organizing imports
+			always_organize_imports = true,
 
-      -- filter diagnostics
-      filter_out_diagnostics_by_severity = {},
-      filter_out_diagnostics_by_code = {},
+			-- filter diagnostics
+			filter_out_diagnostics_by_severity = {},
+			filter_out_diagnostics_by_code = {},
 
-      -- inlay hints
-      auto_inlay_hints = true,
-      inlay_hints_highlight = "Comment",
-      inlay_hints_priority = 200, -- priority of the hint extmarks
-      inlay_hints_throttle = 150, -- throttle the inlay hint request
-      inlay_hints_format = { -- format options for individual hint kind
-        Type = {},
-        Parameter = {},
-        Enum = {},
-        -- Example format customization for `Type` kind:
-        -- Type = {
-        --     highlight = "Comment",
-        --     text = function(text)
-        --         return "->" .. text:sub(2)
-        --     end,
-        -- },
-      },
+			-- inlay hints
+			auto_inlay_hints = true,
+			inlay_hints_highlight = "Comment",
+			inlay_hints_priority = 200, -- priority of the hint extmarks
+			inlay_hints_throttle = 150, -- throttle the inlay hint request
+			inlay_hints_format = { -- format options for individual hint kind
+				Type = {},
+				Parameter = {},
+				Enum = {},
+				-- Example format customization for `Type` kind:
+				-- Type = {
+				--     highlight = "Comment",
+				--     text = function(text)
+				--         return "->" .. text:sub(2)
+				--     end,
+				-- },
+			},
 
-      -- update imports on file move
-      update_imports_on_move = false,
-      require_confirmation_on_move = true,
-      watch_dir = nil,
-    })
+			-- update imports on file move
+			update_imports_on_move = false,
+			require_confirmation_on_move = true,
+			watch_dir = nil,
+		})
 
-    -- required to fix code action ranges and filter diagnostics
-    ts_utils.setup_client(client)
+		-- required to fix code action ranges and filter diagnostics
+		ts_utils.setup_client(client)
 
-    -- no default maps, so you may want to define some here
-    local opts = { silent = true }
+		-- no default maps, so you may want to define some here
+		local opts = { silent = true }
 
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
-    -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":TSLspRenameFile<CR>", opts)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspImportAll<CR>", opts)
-  end,
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
+		-- vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":TSLspRenameFile<CR>", opts)
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspImportAll<CR>", opts)
+	end,
 })
