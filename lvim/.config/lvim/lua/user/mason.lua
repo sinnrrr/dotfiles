ad1e51bbbbfcb9f38444f1d545e5805ff2ba833c
Dmytro <dimasoltusyuk@gmail.com>
2022-10-27 22:48:23 +0200

    feat(lvim): docker hadolint mason default install

--- lvim/.config/lvim/lua/user/mason.lua
+++ lvim/.config/lvim/lua/user/mason.lua
@@ -40,6 +40,7 @@ M.config = function()
 			"flake8",
 			"black",
 			"isort",
+			"hadolint",
 			-- you can pin a tool to a particular version
 			-- { "golangci-lint", version = "1.47.0" },
 			-- you can turn off/on auto_update per tool
