d7d982a0755c055ce11c936f9e9a10759caacf5c
Nick Yasnogorodskyi <nikyasnogorodskyi@gmail.com>
2024-04-09 13:20:40 +0200

    feat: migrate to yabai scratchpad feature

--- roles/dotfiles/files/os_Darwin/yabai/.config/yabai/scripts/quick_terminal_2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-{ kitty --single-instance --instance-group quickterm --title QuickTerminal --directory "${HOME}" tmux new -A -s quickterm & } &> /dev/null
-kitty_pid=$!
-disown -r "${kitty_pid}"
